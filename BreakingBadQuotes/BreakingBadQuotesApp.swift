3f7f444b4e6f07da6bee5e3b3af69a6efd8b0de9
Anup Saud <anupsaud10@gmail.com>
2024-07-17 08:49:31 -0400

    Initial Commit

---
+++ BreakingBadQuotes/BreakingBadQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadQuotesApp.swift
+//  BreakingBadQuotes
+//
+//  Created by Anup Saud on 2024-07-17.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakingBadQuotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BreakingBadQuotes
+//
+//  Created by Anup Saud on 2024-07-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
